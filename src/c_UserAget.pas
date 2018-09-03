a8b3c59a7ad875ac6f0213616756bf865796dae7
zed <starmen@tut.by>
2018-09-03 22:59:02 +0300

    Update User-Agent string

--- src/c_UserAget.pas
+++ src/c_UserAget.pas
@@ -5,7 +5,7 @@ interface
 const
   cBrowserUserAgent =
     'Mozilla/5.0 (Windows NT 6.3; Win64; x64) ' +
-    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36';
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36';
 
   cDesktopClientUserAgent =
     'GoogleEarth/6.2.2.6613(Windows;Microsoft Windows (6.2.9200.0);en;kml:2.2;client:Free;type:default)';
