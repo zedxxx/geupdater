1d4208ad59347560392a9f6a9311c355445be586
zed <starmen@tut.by>
2019-12-05 16:51:21 +0300

    Update chrome user-agent string

--- src/c_UserAget.pas
+++ src/c_UserAget.pas
@@ -6,7 +6,7 @@ const
   cBrowserUserAgent =
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
     'AppleWebKit/537.36 (KHTML, like Gecko) ' +
-    'Chrome/73.0.3683.86' + ' ' +
+    'Chrome/78.0.3904.108' + ' ' +
     'Safari/537.36';
 
   cGoogleEarthClientVersion = '7.3.2.5776';
