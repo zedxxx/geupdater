4df38f3a62e2f6d2b754b8a5c2023e5324b74464
zed <starmen@tut.by>
2026-04-05 15:10:13 +0300

    Add build script

---
+++ Build.Release.cmd
@@ -0,0 +1,11 @@
+
+set app=GEUpdateChecker
+
+call rsvars.bat
+
+for %%p in (Win32 Win64) do (
+  msbuild %app%.dproj "/p:config=Release" "/p:platform=%%p" "/t:rebuild" "/p:VerInfo_AutoIncVersion=false"
+  amResourceModuleBuilder %app%.xlat -b -s:.\.bin\%%p\%app%.exe -y:.\.bin\%%p\%app%.drc -o:.\.bin\%%p\res\ -i -v -n:1
+)
+
+pause
